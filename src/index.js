f2c3e4a37af859fc5bb8cc0376018c7c0cdf51c2
maurya20 <mukesh.ice17@gmail.com>
2020-08-22 09:46:30 +0530

    Myinfo component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Car from './myapp.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<Car />, document.getElementById('root'));
+
+function MyInfo() {
+  return(
+    <div>
+      <h1>My Informatation</h1>
+      <h3>Name: Mukesh Maurya</h3>
+      <h3>Email: mukesh.ice17@gmail.com</h3>
+      <h3>Country: India</h3>
+      <h3>Hobbies: </h3>
+     <ul>
+     <li>Cooking</li>
+     <li>Travelling</li>
+     <li>Talking to people</li>
+     </ul>
+    </div>
+    
+  )
+}
+
+ReactDOM.render(< MyInfo />, document.getElementById("root"))
