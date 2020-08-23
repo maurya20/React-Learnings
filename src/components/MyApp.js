ebb572d5fdcfa0568625bf7cea9b1d5620fc32b3
maurya20 <mukesh.ice17@gmail.com>
2020-08-23 09:02:24 +0530

    Css used and javascript used in JSX.

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -4,11 +4,13 @@ import Header from "./Header"
 
 
 function MyApp() {
+    const Name = "Mukesh Maurya"
+    let y = 34
     return(
       <div>
         < Header />
         <h1>My Informatation</h1>
-        <h3>Name: Mukesh Maurya</h3>
+        <h3>Name: {Name +y}</h3>
         <h3>Email: mukesh.ice17@gmail.com</h3>
         <h3>Country: India</h3>
         <h3>Hobbies: </h3>
