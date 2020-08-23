080faf5ce23f4bdd84f5c3d2fdb1c6b901bbc63a
maurya20 <mukesh.ice17@gmail.com>
2020-08-23 08:28:43 +0530

    Header, main content and footer implemented in MyApp

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+    return(
+      <div>
+        <h3>Logo</h3>
+        <h3>Links</h3>
+        <h3>Menue bar</h3>
+      </div>
+    )
+  }
+
+export default Header
--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Footer from "./Footer"
+import Header from "./Header"
 
 
 function MyApp() {
     return(
       <div>
+        < Header />
         <h1>My Informatation</h1>
         <h3>Name: Mukesh Maurya</h3>
         <h3>Email: mukesh.ice17@gmail.com</h3>
