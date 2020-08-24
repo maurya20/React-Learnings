d82766e9d1cfce269c6119c11392ce4a8b5018ed
maurya20 <mukesh.ice17@gmail.com>
2020-08-24 17:19:48 +0530

    Initial bootstraping

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MyApp from "./MyApp"
-import "./MyApp.css" 
-
-
+import "./MyApp.css"
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 
 ReactDOM.render(< MyApp />, document.getElementById("root"))
