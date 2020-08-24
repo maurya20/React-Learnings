82cd8923a769f6cdccd368ff8d7445583f7140e7
maurya20 <mukesh.ice17@gmail.com>
2020-08-24 17:47:21 +0530

    Main component with bootstrap style.

--- src/Blog.js
+++ src/Blog.js
@@ -1,15 +1,26 @@
 import React from "react"
 
+import Card from 'react-bootstrap/Card'
+import { CardGroup } from 'react-bootstrap';
+
 function Blog(props) {
     return(
-      <div className="blog">
-        <p>Date:{props.date}</p>
-        <h3>Category:{props.category}</h3>
-        <h3>Heading:{props.heading}</h3>
-        <img src={props.pic} alt="Only text blog"></img>
-        
-        
-      </div>
+
+<CardGroup>
+  <Card>
+    <Card.Img variant="top" src={props.pic} />
+    <Card.Body>
+      <Card.Title>{props.heading}</Card.Title>
+      <Card.Text>
+      {props.category}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">{props.date}</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
+
     )
   }
 
