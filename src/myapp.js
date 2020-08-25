b0cad30e204a95fe307b8aeb9d75fe40523d83b2
maurya20 <mukesh.ice17@gmail.com>
2020-08-25 09:57:43 +0530

    Class component wit state and some logic to be used.

--- src/myapp.js
+++ src/myapp.js
@@ -1,11 +1,18 @@
 import React from "react"
 
 class App1 extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name : "Mukesh"
+    }
+  }
   render() {
     const date = new Date()
     const hour = date.getDate()
     return (
       <div className="footer">
+        <h1>Hello, My Name is {this.state.name}</h1>
         <h1>Follow us on  {hour}</h1>
         <ul>
           <li>Instagram</li>
