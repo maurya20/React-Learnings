6e8cf359156f84252a83a41f4ef22c1de6e6eec7
maurya20 <mukesh.ice17@gmail.com>
2020-08-25 11:08:39 +0530

    counter implemented using prevState.

--- src/myapp.js
+++ src/myapp.js
@@ -4,20 +4,24 @@ class App1 extends React.Component {
   constructor() {
     super()
     this.state = {
-      isLogedIn : false
+      count: 0
     }
+      this.counter= this.counter.bind(this)
+    
+  }
+  counter() {
+    this.setState(prevState=> {
+      return {
+        count: prevState.count + 1
+      }
+    })
   }
   render() {
-    let wd
-    if (this.state.isLogedIn===true){
-      wd = "In"
-    }
-    else {
-      wd = "out"
-    }
+    
     return (
       <div className="footer">
-        <h1>Hello, there I am loged {wd} now.</h1>
+        <h1>{this.state.count}</h1>
+        <button onClick= {this.counter} style={{color:'red'}}>Count</button>
         <h1>Follow us on</h1>
         <ul>
           <li>Instagram</li>
