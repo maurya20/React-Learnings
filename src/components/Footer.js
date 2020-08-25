6a4a91b9501226697e757bff4aa9b6b879e36375
maurya20 <mukesh.ice17@gmail.com>
2020-08-25 19:34:07 +0530

    Footer component by material design.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,46 @@
-import React from "react"
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
-function Footer() {
-    return(
-      
-      <div class="footer">
-        <h5> M & M Enterprises</h5>
-        <p>Mob- 9540339805</p>
-        <p>Email- mukesh.ice17@gmail.com</p>
-        <h5> M & M Enterprises</h5>
-        <p>Mob- 9540339805</p>
-        <p>Email- mukesh.ice17@gmail.com</p>
+const Footer = () => {
+  return (
+    <div className="footer">
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">M & M Enterprises</h5>
+            <p>
+              Email: mukesh.ice17@gmail.com
+            </p>
+            <p>Mob: 9540339805</p>
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">Link 1</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 2</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 3</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Link 4</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://github.com/maurya20"> Github </a>
+        </MDBContainer>
       </div>
-    )
-  }
+    </MDBFooter>
+    </div>
+  );
+}
 
 export default Footer
