853282a6c2bbc3a43dbdefef5d6f691ed4f72956
maurya20 <mukesh.ice17@gmail.com>
2020-09-23 17:09:13 +0530

    Header dynamic style on input change.

--- src/components/main.js
+++ src/components/main.js
@@ -4,14 +4,37 @@ import NewsFeed from './NewsFeed'
 
 export class Main extends Component {
   state={
-    news:JSON
+    news:JSON,
+    input:""
   }
+  inputChangeHandler = (event)=>{
+    this.setState({
+      input: event.target.value
+    }
+
+    )
+  }
+  
+
+  
   render() {
-    console.log(JSON)
+    let styling = ()=>{
+      if(this.state.input==="")
+      return "red"
+      else
+      return "skyblue"
+    }
+    const red= "red"
+    const skyblue= "skyblue"
     return (
       <div>
+        <div style={{backgroundColor: styling(), height:90}}>
+          <h1>My Logo</h1>
+          
+        </div><br></br>
+        <input type="text" onChange={this.inputChangeHandler}/>
         <NewsFeed news={this.state.news}>
-        <h1>Your feeds are here !!!</h1>
+    <h1>Mr. {this.state.input}, your feeds are here !!!</h1>
         </NewsFeed>
       </div>
     )
