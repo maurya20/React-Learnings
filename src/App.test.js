dd183f99109fa0f5b29ce950c4f4c5b8daab42ae
maurya20 <mukesh.ice17@gmail.com>
2020-09-30 10:27:54 +0530

    stop tracking these files.

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Car from './myapp.js';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Car />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
