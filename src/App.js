1903b8825f89e4d06b1d5af6ca860c51dffb9867
maurya20 <mukesh.ice17@gmail.com>
2020-10-23 11:20:29 +0530

    Logged state changed in different component and action is dispatched from different component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react"
-import {useSelector,useDispatch} from 'react-redux'
-import {increment,decrement,logAction} from './actions'
+import { useSelector } from "react-redux";
+
 
 
 const App = ()=>{
-const counter = useSelector(state=>state.counter)
-const isLogged = useSelector( state=>state.isLogged)
-const dispatch = useDispatch()
+    const isLogged = useSelector((state) => state.isLogged);
     return (
-            <div>
+            <div style={{background:"yellow"}}>
                 <h1>Hello Redux!!!!!!</h1>
-               <h3>Counter: {counter.count}</h3>
-<button onClick={()=>dispatch(increment(5))}>+</button>
-<button onClick={()=>dispatch(decrement())}>-</button>
-<button onClick={()=>dispatch(logAction())}>Log-Action</button>
-
-               {isLogged? <h3>LogIn</h3>:<h3>LogOut</h3>}
+                {isLogged ? <h3>LogIn</h3> : <h3>LogOut</h3>}
+              
     
 
             </div>)
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import {useSelector,useDispatch} from 'react-redux'
-import {increment,decrement,logAction} from '../actions'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, logAction } from "../actions";
 const Home = () => {
+  const counter = useSelector((state) => state.counter);
+ 
+  const dispatch = useDispatch();
 
-    const counter = useSelector(state=>state.counter)
-    const isLogged = useSelector( state=>state.isLogged)
-    const dispatch = useDispatch()
+  return (
+    <div>
+      <h1>Hello from Home Component!!!!</h1>
+      <h3>Counter: {counter.count}</h3>
+      <button className="btn btn-primary btn-lg btn-block" onClick={() => dispatch(increment(5))}>+</button>
+      <button className="btn btn-primary btn-lg btn-block"  onClick={() => dispatch(decrement())}>-</button>
+      <button  className="btn btn-primary btn-lg btn-block" onClick={() => dispatch(logAction())}>Log-Action</button>
 
-    return (
-        <div>
-            <h1>Hello from Home Component!!!!</h1>
-               <h3>Counter: {counter.count}</h3>
-<button onClick={()=>dispatch(increment(5))}>+</button>
-<button onClick={()=>dispatch(decrement())}>-</button>
-<button onClick={()=>dispatch(logAction())}>Log-Action</button>
+      
+    </div>
+  );
+};
 
-               {isLogged? <h3>LogIn</h3>:<h3>LogOut</h3>}
-        </div>
-    )
-}
-
-export default Home
+export default Home;
