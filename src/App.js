cf511fdecb698d676094507ca665dd2d6816e369
maurya20 <mukesh.ice17@gmail.com>
2021-02-21 08:41:13 +0530

    config 1st test using only jest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+export const greet = (name) => {
+  return "Welcome" + " " + name;
+};
 function App() {
   return (
     <div className="App">
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { greet } from "./App";
 
-test('renders learn react link', () => {
+it("", () => {
+  const result = greet("Tribiyani");
+  expect(result).toMatch("Welcome Tribiyani");
+});
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
